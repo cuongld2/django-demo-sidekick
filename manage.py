f0953d5459cf8cbaaa6ecd382f6b90613110a69e
Donald Le <donald.le@iamondemand.com>
2022-04-15 23:45:10 +0700

    Demo code for runsidekick

---
+++ manage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == '__main__':
+    from pathlib import Path
+    from dotenv import load_dotenv
+
+    env_path = Path('config.env').resolve()
+    load_dotenv(dotenv_path=env_path)
+    import tracepointdebug
+
+    try:
+        tracepointdebug.start()
+    except ImportError as e:
+        pass
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings.development')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
